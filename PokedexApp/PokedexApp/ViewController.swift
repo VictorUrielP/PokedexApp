e4a0eea0977c4ea6e9fe3f442f2f5f4f4d9019c7
Uriel Pacheco <uriel.pacheco@wizeline.com>
2020-12-09 12:07:03 -0600

    Initial commit. Add project.

---
+++ PokedexApp/PokedexApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexApp
+//
+//  Created by Victor Uriel Pacheco Garcia on 09/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
